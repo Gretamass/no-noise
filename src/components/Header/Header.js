83ab5d950e619b037621478a830063bdf311cdcd
Greta Masiulyte <gretos.masiulytes@gmail.com>
2022-02-05 12:22:28 +0200

    a tags switched to Link tags

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
+
 import logo from '../images/logo.png';
 
 const Header = () => {
@@ -7,18 +9,18 @@ const Header = () => {
     <div className='header'>
         <div className='container headerContainer'> 
             <div className='logo'>
-                <img src={logo} alt="no noise logo" className='logoHeader' />
+                <Link to="/"><img src={logo} alt="no noise logo" className='logoHeader' /></Link>
             </div>
 
             <div className='navbar'>
-                <div className='navList'><a href="/news">Naujienos</a></div>
-                <div className='navList'><a href="/aboutus">Apie Mus</a></div>
-                <div className='navList'><a href="/services">Paslaugos</a></div>
-                <div className='navList'><a href="/sales">Prekyba</a></div>
-                <div className='navList'><a href="/clients">Mūsų Klientai</a></div>
-                <div className='navList'><a href="/projects">Įgyvendinti projektai</a></div>
-                <div className='navList'><a href="/contacts">Kontaktai</a></div>
-                <div className='navList'><a href="/eng">ENG</a></div>
+                <div className='navList'><Link to="/news">Naujienos</Link></div>
+                <div className='navList'><Link to="/aboutus">Apie Mus</Link></div>
+                <div className='navList'><Link to="/services">Paslaugos</Link></div>
+                <div className='navList'><Link to="/sales">Prekyba</Link></div>
+                <div className='navList'><Link to="/clients">Mūsų Klientai</Link></div>
+                <div className='navList'><Link to="/projects">Įgyvendinti projektai</Link></div>
+                <div className='navList'><Link to="/contacts">Kontaktai</Link></div>
+                <div className='navList'><Link to="/eng">ENG</Link></div>
 
             </div>
         </div>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import image1 from '../images/main1.png';
 import image2 from '../images/main2.png';
 import image3 from '../images/main3.png';
+import { Link } from 'react-router-dom';
 
 import './Hero.css';
 import '../../App.css';
@@ -11,13 +12,13 @@ const Hero = () => {
       <div className="container heroContainer">
           <div className="photoContainer">
             <div className="heroPhoto">
-                <a  href="/design"><img src={image1} alt="no noise hero" className='heroPhoto'/></a>
+                <Link  to="/design"><img src={image1} alt="no noise hero" className='heroPhoto'/></Link>
             </div>
             <div className="heroPhoto">
-                <a  href="/sales"><img src={image2} alt="no noise hero" className='heroPhoto'/></a>
+                <Link  to="/sales"><img src={image2} alt="no noise hero" className='heroPhoto'/></Link>
             </div>
             <div className="heroPhoto">
-                <a  href="/rental"><img src={image3} alt="no noise hero" className='heroPhoto'/></a>
+                <Link  to="/rental"><img src={image3} alt="no noise hero" className='heroPhoto'/></Link>
             </div>
            
           </div>
